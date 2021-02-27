208306e74394b9511b14fcb5dd67d9cde308ab10
Burian Sandor <buriansandor95@gmail.com>
2021-02-27 15:30:13 +0100

    kapu nyitás és tehén kijön

--- cameramovement.js
+++ cameramovement.js
@@ -12,8 +12,18 @@ window.onload = function() {
 	setTimeout(function(){
     //do what you need here
 
-		/*document.getElementById('cam').setAttribute('animation', 'property: position; to: 1.291 1 3.099');
-		document.getElementById('cam').setAttribute('animation', 'property: rotation; to: -15.699 26.929 0');*/
-		document.getElementById('fejtarto').setAttribute('animation', 'property: rotation; to: -90 0 0');
-	}, 7000);
+		//document.getElementById('cam').setAttribute('animation', 'property: position; to: 1.291 1 3.099');
+		//document.getElementById('cam').setAttribute('animation', 'property: rotation; to: -15.699 26.929 0');
+
+		document.getElementById('fejtarto').setAttribute('animation', 'property: rotation; to: 0 105 0');
+	}, 9000);
+	setTimeout(function(){
+    //do what you need here
+
+		//document.getElementById('cam').setAttribute('animation', 'property: position; to: 1.291 1 3.099');
+		//document.getElementById('cam').setAttribute('animation', 'property: rotation; to: -15.699 26.929 0');
+
+	document.getElementById('maincow').setAttribute('animation', 'property: position; to: -1.150 -0.086 2.980');
+	document.getElementById('cam').setAttribute('animation', 'property: position; to: 3.5 1 7');
+	}, 12000);
 };
